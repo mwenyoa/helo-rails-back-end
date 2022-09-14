1215eb3b851bce75bd79faa43ddf57b14952b959
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-14 21:01:21 +0200

    api app setup files

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# creatte a Message
+Greeting.create!(message: "First Message!")
+Greeting.create!(message: "Second Message!")
+Greeting.create!(message: "Third Message!")
+Greeting.create!(message: "Fourth Message!")
+Greeting.create!(message: "Fifth Message!")
+Greeting.create!(message: "Sixth Message!")
+Greeting.create!(message: "Seventh Message!")
+Greeting.create!(message: "Eighth Message!")
+Greeting.create!(message: "Ninth Message!")
